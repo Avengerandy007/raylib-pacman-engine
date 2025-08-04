35a521d4c119bea86bb7c80e8696405a79a36ebd
Avengerandy007 <Avengerandy007@users.noreply.github.com>
2025-08-04 20:17:53 +0300

    More structuring ;)

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -1,6 +1,7 @@
 #include "../Raylib/include/raylib.h"
+#include <memory>
 class Entity{
-	Rectangle rect;
+	std::unique_ptr<Rectangle> rect = std::make_unique<Rectangle>();
 public:
-
+	virtual void Update();
 };
---
+++ include/EntityModules.hpp
@@ -0,0 +1,31 @@
+#include "../Raylib/include/raylib.h"
+#include <bits/types/FILE.h>
+#include <memory>
+#include <string>
+
+class Controller{
+	int speed;
+public:
+	Vector2 dir;
+
+	void Move(Vector2& pos);
+};
+
+class TileCollider{
+public:
+	bool Colliding(std::unique_ptr<Rectangle> myRect);
+};
+
+class ImageTexture{
+	std::string path;
+	Texture2D texture;
+public:
+	ImageTexture();
+	void Render(Rectangle rect);
+	~ImageTexture();
+};
+
+class InputHandler{
+public:
+	void ChangeDirection(Vector2& dir);
+};
