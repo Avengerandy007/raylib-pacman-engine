b1cf65b524be2bafa8ff7ec49fa19a8ca256064c
Avengerandy007 <Avengerandy007@users.noreply.github.com>
2025-09-13 12:41:10 +0300

    Allow for different sized matrices

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -5,13 +5,13 @@
 
 Matrix2<Tile, 20> Tile::tileSet;
 
-void Tile::InitTileSet(const uint8_t size){
+void Tile::InitTileSet(const uint8_t tileSize, const uint16_t setSize){
 	uint8_t x = 0, y = 0;
-	for (; x < tileSet.matrix.size(); x++){
+	for (; x < setSize; x++){
 		y = 0;
-		for (; y < tileSet.matrix[x].size(); y++){
+		for (; y < setSize; y++){
 			tileSet.matrix[x][y].m_def = std::make_shared<Rectangle>();
-			tileSet.matrix[x][y].SetPosition(x, y, size);
+			tileSet.matrix[x][y].SetPosition(x, y, tileSize);
 		}
 	}
 }
@@ -23,9 +23,9 @@ void Tile::SetPosition(uint8_t x, uint8_t y, uint8_t size) const{
 	m_def->height = size;
 }
 
-void Tile::SetEntityRectangles(){
-	for (uint8_t i = 0; i < 20; i++){
-		for(uint8_t k = 0; k < 20; k++){
+void Tile::SetEntityRectangles(const uint16_t matrixSize){
+	for (uint8_t i = 0; i < matrixSize; i++){
+		for(uint8_t k = 0; k < matrixSize; k++){
 			if (Tile::tileSet.matrix[i][k].m_containedEntity) Tile::tileSet.matrix[i][k].m_containedEntity->rect = Tile::tileSet.matrix[i][k].m_def;
 			if (Tile::tileSet.matrix[i][k].m_coinContainer) Tile::tileSet.matrix[i][k].m_coinContainer->rect = Tile::tileSet.matrix[i][k].m_def;
 		}
