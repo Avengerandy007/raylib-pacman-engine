0e1174a3a9bcbe5ecd5df2a56a70a3c52ddad9e3
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-09-14 22:27:44 +0300

    Create tile sets of any size

--- include/FileI.hpp
+++ include/FileI.hpp
@@ -1,14 +1,55 @@
 #include "../include/Matrix.hpp"
 #include "../include/Tile.hpp"
-#include <cstdint>
-#include <cstdio>
+#include <cstddef>
 #include <iostream>
+#include <cstdint>
 #include <string>
 #include <fstream>
+
 namespace FileI{
 	extern std::ifstream FindFile();
 	extern std::string FileContent(std::ifstream& file);
 	extern uint16_t GetCoinCount(const std::string& fileContent, uint16_t& currentChar);
-	extern Matrix2<Tile, 20> CreateTileSet(std::string& fileContent, const uint16_t startChar);
+
+	template<size_t matrixSize>
+	Matrix2<Tile, matrixSize> CreateTileSet(std::string& fileContent, const uint16_t startChar){
+		Matrix2<Tile, matrixSize> matrix;
+				
+		uint16_t currentChar = startChar;
+
+		for (uint32_t i = 0; i < matrixSize; i++){
+			for (uint32_t k = 0; k < matrixSize; k++){
+				switch (fileContent[currentChar]) {
+					case '0':
+						break;
+
+					case '1':
+						matrix.matrix[i][k].m_containedEntity = std::make_unique<Wall>();
+						break;
+
+					case '2':
+						matrix.matrix[i][k].m_containedEntity = std::make_unique<Player>(i, k);
+						break;
+
+					case '3':
+						matrix.matrix[i][k].m_coinContainer= std::make_unique<Coin>();
+						break;
+					case '4':
+						matrix.matrix[i][k].m_containedEntity = std::make_unique<Ghost>(i, k);
+						break;
+
+					default:
+						std::cout << "Unrecognized char " << fileContent[currentChar] << "\n";
+						i--;
+						k--;
+						break;
+
+				}
+				currentChar++;
+			}
+		}
+		std::cout << "Finished creating matrix\n";
+		return matrix;
+	}
 	extern Matrix2<Tile, 20> MakeMatrix(std::ifstream& file);
 }
